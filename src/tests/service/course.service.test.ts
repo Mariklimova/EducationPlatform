c425a59b677d188c7e43167bb85b59b954c634ef
Mariklimova <mariku22@yandex.ru>
2024-03-16 14:48:16 +0300

    completed the service tests

--- src/tests/service/course.service.test.ts
+++ src/tests/service/course.service.test.ts
@@ -1,4 +1,4 @@
-import { createCourse, getAllCourse, getCourseById } from "../../service/course.service";
+import { createCourse, getAllCourse, getCourseById, updateCourse, deleteCourse, partUpdateCourse } from "../../service/course.service";
 import * as repository from '../../repository/course.repository';
 
 describe('createCourse', () => {
@@ -71,21 +71,65 @@ describe('getCourseById', () => {
 describe('updateCourse', () => {
     test('corrected', async () => {
         const mock = jest.spyOn(repository, 'updateCourseDB');
+        mock.mockResolvedValue([{ id: 1, course: 'JavaScript', description: 'test1' }]);
+        const result = await updateCourse('1', 'JavaScript', 'test1');
+        expect(mock).toHaveBeenCalled();
+        expect(result).toEqual([{ id: 1, course: 'JavaScript', description: 'test1' }]);
+        expect(result).toHaveLength(1);
+    });
+
+    test('uncorrected', async () => {
+        const mock = jest.spyOn(repository, 'updateCourseDB');
+        mock.mockResolvedValue([]);
+        try {
+            await updateCourse('1', 'JavaScript', 'test1');
+        } catch (error: any) {
+            expect(mock).toHaveBeenCalled();
+            expect(error.message).toBe('Data is not saved');
+        }
+    });
+});
+
+describe('deleteCourse', () => {
+    test('corrected', async () => {
+        const mock = jest.spyOn(repository, 'deleteCourseDB');
         mock.mockResolvedValue([{ id: 2, course: 'TS', description: 'test2' }]);
-        const result = await getCourseById('2');
+        const result = await deleteCourse('2');
         expect(mock).toHaveBeenCalled();
         expect(result).toEqual([{ id: 2, course: 'TS', description: 'test2' }]);
         expect(result).toHaveLength(1);
     });
 
     test('uncorrected', async () => {
-        const mock = jest.spyOn(repository, 'getAllCourseDB');
+        const mock = jest.spyOn(repository, 'deleteCourseDB');
         mock.mockResolvedValue([]);
         try {
-            await getCourseById('2');
+            await deleteCourse('2');
         } catch (error: any) {
             expect(mock).toHaveBeenCalled();
-            expect(error.message).toBe('Id is not found');
+            expect(error.message).toBe('Id is not found, data was not deleted');
+        }
+    });
+});
+
+describe('partUpdateCourse', () => {
+    test('corrected', async () => {
+        const mock = jest.spyOn(repository, 'partUpdateCourseDB');
+        mock.mockResolvedValue([{ id: 1, course: 'JavaScript', description: 'test_1' }]);
+        const result = await partUpdateCourse('1', { description: 'test_1' });
+        expect(mock).toHaveBeenCalled();
+        expect(result).toEqual([{ id: 1, course: 'JavaScript', description: 'test_1' }]);
+        expect(result).toHaveLength(1);
+    });
+
+    test('uncorrected', async () => {
+        const mock = jest.spyOn(repository, 'partUpdateCourseDB');
+        mock.mockResolvedValue([]);
+        try {
+            await partUpdateCourse('1', { description: 'test_1' });
+        } catch (error: any) {
+            expect(mock).toHaveBeenCalled();
+            expect(error.message).toBe('Data is not changed');
         }
     });
 })
